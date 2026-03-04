b199d06867c07438ecfa0d252ca0a1cfe61161cc
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-04 23:56:15 +0530

    Update practice_problem1.py

--- practice_problem1.py
+++ practice_problem1.py
@@ -2,4 +2,27 @@
 
 a = 5 
 b = 6
+
 print(a+b)
+
+# print("hello world")
+
+a = 5 
+b = 6
+print(a+b)
+
+n = 5 
+for i in range(1, 11):
+    print(f"{n} x {i} = ", n*i)
+
+# P (Principle) = initial amount borrowed
+# r (Rate) = Annual Rate 
+# t (Time) =  Time period in years
+
+def simple_interest(P, r, t):
+    r = r/100
+    SI = P*r*t
+    return SI
+
+a = simple_interest(10000, 5, 3)
+print(a)
